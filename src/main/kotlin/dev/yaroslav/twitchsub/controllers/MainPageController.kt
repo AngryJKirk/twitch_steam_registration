c7277d739f39bee2c0da466e561639808e768940
Yaroslav <angrynaz@gmail.com>
2020-11-14 23:02:30 +0300

    add reset controller

--- src/main/kotlin/dev/yaroslav/twitchsub/controllers/MainPageController.kt
+++ src/main/kotlin/dev/yaroslav/twitchsub/controllers/MainPageController.kt
@@ -4,6 +4,7 @@ import dev.yaroslav.twitchsub.services.StateService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import javax.servlet.http.HttpServletResponse
 import javax.servlet.http.HttpSession
 
 @Controller
@@ -19,5 +20,11 @@ class MainPageController(private val stateService: StateService) {
                 model.addAttribute(it.key.id, it.value.id)
             }
         return "index"
+    }
+
+    @GetMapping("/reset")
+    fun reset(httpServletResponse: HttpServletResponse, httpSession: HttpSession){
+        httpSession.invalidate()
+        httpServletResponse.sendRedirect("/")
     }
 }
