f6bc57479e7448dd6441d80ff87976410ab837ac
Yaroslav <angrynaz@gmail.com>
2020-11-14 22:37:17 +0300

    fix required command in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
 ./gradlew clean build -x test &&
 docker build . -f Dockerfile -t wprot-rp &&
-docker kill $(docker ps -q --filter publish=8080 --format="{{.ID}}") &&
+docker kill $(docker ps -q --filter publish=8080 --format="{{.ID}}")
 docker run -d --restart always -p 127.0.0.1:8080:8080 wprot-rp:latest
